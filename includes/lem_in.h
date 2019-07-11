094ad1b0a73de7247cb1fdb1988008c475168607
Tweety-7 <49977306+Tweety-7@users.noreply.github.com>
2019-07-11 17:42:02 +0300

    Update lem_in.h

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -19,13 +19,24 @@
 # include "libft.h"
 # include "ft_printf.h"
 
+//односвязный список, в таком хранятся комнаты, кротчайший путь, очередь обхода.
 typedef struct		s_list_i
 {
 	int			content;
 	struct s_list_i	*next;
 }					t_list_i;
 
+//для построение связей между комнатами.
+//вниз обход по всем комнатам, в контенте соответсвенно хранится номер комнаты
+typedef struct			s_list_down
+{
+	int					content;
+	struct s_list_i		*next; // из каждого столбца в право начинается лист.
+	struct s_list_down	*down;
 
+}						t_list_down;
+
+//структура каждой комнаты
 typedef struct		s_room
 {
 	int				x;//координаты комнаты
@@ -42,23 +53,54 @@ typedef struct		s_map
 {
 	int				fd;
 	int				c_room;//кол - во комнат
-	t_list_i			**matr;//списки смежности
+	int 			c_ant;//кол-во муравьев
+	t_list_down		*link;//лист - столбец для указания связей
 	t_room			*rooms;//лист всех комнат
-	t_list_i		*sh;
+	t_list_i		*sh;//кротчайший путь
 }					t_map;
 
-
-
+	// (valid.c)
+// ф., которые проверяют на валидность  - возвращают 1 если все хорошо,
+// и 0 еслигде то эрор
+//проверяет на валидность первую строку, инициализируя структуру map
+int 	make_map(int fd, t_map *map);
+//проверяет на валидность список комнат, заполняя лист room,
+// пока не нашли первую ссылку
 int	check_room(t_map *map);
-int		ft_len_int(int num);
-t_room	*ft_create_ele(char *line, int number);
-void ft_pushback(t_room **head, t_room *new);
+
+int created_links(char *line, t_map *map);
+//записывает по две связи для каждой комнаты
+int check_links(char *line, t_map *map);
+
+
+
+
 int ft_bfs(t_map *map);
 
 
 
+	// (liba.c)
+//ф, которые претендуют на звание стандартных 
+int		ft_len_int(int num);
 
+	//(list_room.c)
+//ф для списка комнат
+t_room	*ft_create_ele(char *line, int number);
+void ft_pushback(t_room **head, t_room *new);
+void	ft_lstadd_r(t_room **alst, t_room *new);
 
+	//ft_list_down.c
+t_list_i *ft_list_i_head(int num, t_list_down **alst);
+t_list_down	*ft_lstnew_down(int content);
+void	ft_lstaddback_down(t_list_down **alst, t_list_down *new);
+
+	// (ft_list_i.c)
+//ф для работы с односвязным списком
 t_list_i	*ft_lstnew_i(int content);
 void	ft_lstaddback_i(t_list_i **alst, t_list_i *new);
+
+void	ft_remove_list_if(t_list_i **head, int content);
+void	ft_add_list_if(t_list_i **head, int content, t_list_i *new);
+void	ft_add_list_if(t_list_i **head, int content, t_list_i *new);
+void	ft_list_revers(t_list_i **begin_list);
 #endif
