bb22189f98608ca6d023d16dc7460819e8c4aa6b
Tweety-7 <49977306+Tweety-7@users.noreply.github.com>
2019-07-19 10:42:35 +0300

    Update lem_in.h

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -149,6 +149,8 @@ void pr_list(t_list_i *new);
 
 // suurballe.c
 void ft_new_room(t_map *map, t_list_i *sh);
+
+// удаляет инверсные пути в link_new
 void ft_del_shared_path(t_map *map, t_list_i *sh, t_list_i *rev_2);
 	//path.c
 void main_path(t_map *map);
