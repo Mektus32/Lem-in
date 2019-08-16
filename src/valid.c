8ff95f470f7d213d7558c09b410a90a6a3918c16
qgilbert <arinkka7@mail.ru>
2019-08-16 21:24:04 +0300

    mmm

--- src/valid.c
+++ src/valid.c
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   valid.c                                            :+:      :+:    :+:   */
+/*   avalid.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojessi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: qgilbert <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/14 15:05:39 by ojessi            #+#    #+#             */
-/*   Updated: 2019/07/14 15:05:46 by ojessi           ###   ########.fr       */
+/*   Created: 2019/08/16 21:05:57 by qgilbert          #+#    #+#             */
+/*   Updated: 2019/08/16 21:06:00 by qgilbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
-// заполним начальнуб струкруру map
-// проверим на валидность первую строку (кол-во муравьев)
-// ? если муравьев 0 кидать ошибку?
-int make_ant(t_map *map)
+
+int				make_ant(t_map *map)
 {
 	char	*line;
 	int		i;
@@ -23,7 +21,7 @@ int make_ant(t_map *map)
 	line = ft_check_cmd(&map->fd);
 	if (line && !(ft_strequ(line, "##start") || ft_strequ(line, "##end")))
 	{
-    	i = 0;
+		i = 0;
 		while (line[i] >= '0' && line[i] <= '9')
 			i++;
 		if ((int)ft_strlen(line) == i)
@@ -40,7 +38,7 @@ int make_ant(t_map *map)
 	return (is_not_valid("no find ant_count\n"));
 }
 
-static int is_elem(char *line)
+static int		is_elem(char *line)
 {
 	int		i;
 
@@ -67,82 +65,81 @@ static int is_elem(char *line)
 }
 
 /*
-Пока есть гнл возможны случаи:
-1. нам дана комната
-2. нам дано старт-энд положение комнаты
-3. комент #
-4. cсылка
-
-комнаты могут следовать в любом порядке. так что выхода кроме как запомнить
-первую и последнюю я не нашла.
-новую комнату когда находим ставим её в конец,
-последнюю комнату ставим в конец, когда комнаты закончались и начались ссылки
-комнату старт ставим в начало
-
-если нет или первой или последней комнаты возвращаем 0
+**Пока есть гнл возможны случаи:
+**1. нам дана комната
+**2. нам дано старт-энд положение комнаты
+**3. комент #
+**4. cсылка
+**комнаты могут следовать в любом порядке. так что выхода кроме как запомнить
+**первую и последнюю я не нашла.
+**новую комнату когда находим ставим её в конец,
+**последнюю комнату ставим в конец, когда комнаты закончались и начались ссылки
+**комнату старт ставим в начало
+**если нет или первой или последней комнаты возвращаем 0
 */
 
-static int start_room(t_map *map, t_valid *val_id, char *line)
+static int		start_room(t_map *map, t_valid *val_id, char *line)
 {
-    val_id->start = val_id->end != 1 ? 1 : 4;
-    ft_str_print_del(&line);
-    line = ft_check_cmd(&map->fd);
-    if (val_id->start == 1 && line && is_elem(line))
-    {
-        ft_list_add_room(&map->rooms, ft_create_ele(line, 0));
-        val_id->start = 2;
-    }
-    else
-        return(is_not_valid("start room_error"));
-    return (0);
+	val_id->start = val_id->end != 1 ? 1 : 4;
+	ft_str_print_del(&line);
+	line = ft_check_cmd(&map->fd);
+	if (val_id->start == 1 && line && is_elem(line))
+	{
+		ft_list_add_room(&map->rooms, ft_create_ele(line, 0));
+		val_id->start = 2;
+	}
+	else
+		return (is_not_valid("start room_error"));
+	return (0);
 }
 
-static t_room  *end_room(t_map *map, t_valid *val_id, char *line)
+static t_room	*end_room(t_map *map, t_valid *val_id, char *line)
 {
-    t_room	*last_room;
+	t_room	*last_room;
 
-    last_room = NULL;
-    val_id->end = val_id->start != 1 ? 1 : 4;
-    ft_str_print_del(&line);
-    line = ft_check_cmd(&map->fd);
-    if (val_id->end == 1 && line && is_elem(line))
-    {
-        last_room = ft_create_ele(line, -1);
-        val_id->end = 2;
-    }
-    else
-        is_not_valid("end room_error");
-    return (last_room);
+	last_room = NULL;
+	val_id->end = val_id->start != 1 ? 1 : 4;
+	ft_str_print_del(&line);
+	line = ft_check_cmd(&map->fd);
+	if (val_id->end == 1 && line && is_elem(line))
+	{
+		last_room = ft_create_ele(line, -1);
+		val_id->end = 2;
+	}
+	else
+		is_not_valid("end room_error");
+	return (last_room);
 }
 
-int	check_room(t_map *map)
+int				check_room(t_map *map)
 {
 	char	*line;
 	t_room	*last_room;
 	t_valid *val_id;
 
-    val_id = (t_valid*)malloc(sizeof(t_valid));
-    val_id->tek_num = 1;
-    val_id->start = 0;
-    val_id->end = 0;
+	val_id = (t_valid*)malloc(sizeof(t_valid));
+	val_id->tek_num = 1;
+	val_id->start = 0;
+	val_id->end = 0;
 	last_room = NULL;
 	while ((line = ft_check_cmd(&map->fd)))
-    {
-	    if (val_id->start == 0 && ft_strequ(line, "##start"))
-            start_room(map, val_id, line);
-	    else if (val_id->end == 0 && ft_strequ(line, "##end"))
-            last_room = end_room(map, val_id, line);
-	    else if (last_room && ft_strchr(line, '-') && !ft_strchr(line, ' '))
-        {
-            last_room->number = val_id->tek_num;
-            ft_push_back_room(&map->rooms, last_room);
-            map->c_room = val_id->tek_num;
-            return (created_links(line, map, val_id));
-        }
-	    else if (is_elem(line))
-            ft_push_back_room(&map->rooms, ft_create_ele(line, val_id->tek_num++));
-	    else
-	        return (is_not_valid("room_error"));
-    }
+	{
+		if (val_id->start == 0 && ft_strequ(line, "##start"))
+			start_room(map, val_id, line);
+		else if (val_id->end == 0 && ft_strequ(line, "##end"))
+			last_room = end_room(map, val_id, line);
+		else if (last_room && ft_strchr(line, '-') && !ft_strchr(line, ' '))
+		{
+			last_room->number = val_id->tek_num;
+			ft_push_back_room(&map->rooms, last_room);
+			map->c_room = val_id->tek_num;
+			return (created_links(line, map, val_id));
+		}
+		else if (is_elem(line))
+			ft_push_back_room(&map->rooms,
+					ft_create_ele(line, val_id->tek_num++));
+		else
+			return (is_not_valid("room_error"));
+	}
 	return (0);
 }
--- src/valid_link.c
+++ src/valid_link.c
@@ -1,80 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   avalid_link.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qgilbert <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/16 21:12:50 by qgilbert          #+#    #+#             */
+/*   Updated: 2019/08/16 21:12:52 by qgilbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "lem_in.h"
 
 /*
-*ф. вызывается для каждой входной строки, где есть - и нет ' '
-*заполняет ссылки в две стороны
-*если не нашли одну из комнат - возвращает 0
-*start = map->rooms; - //находим комнату из строки до '-' во всем списке комнат
+** ф. вызывается для каждой входной строки, где есть - и нет ' '
+** заполняет ссылки в две стороны
+**если не нашли одну из комнат - возвращает 0
+**start = map->rooms; - //находим комнату из строки до '-' во всем списке комнат
+**пока не нашли название комнаты которая в лайн переходим по списку комнат
+**надо что б len симолов в строке совпадали и комнате, а следующий был '-'
+** start - комната до знака '-' => s
+** tmp -  комната после '-' => t
+** len - длина имени первой комнаты =>l
+** f - first
+**se - second
 */
-int check_links(char *line, t_map *map)
-{
-    t_room			*start; // комната до знака '-'
-    t_room			*tmp; // комната после '-'
-    int				len; // длина имени первой комнаты
-    t_list_down		*first;
-    t_list_down		*second;
 
-    start = map->rooms;
-    while (start)
-    {
-        len = ft_strlen(start->name);
-        //пока не нашли название комнаты которая в лайн переходим по списку комнат
-        // надо что б len симолов в строке совпадали и комнате, а следующий был '-'
-        if (ft_strnequ(start->name, line, len) && ft_strnequ("-", line + len, 1))
-        {
-            //комнату до '-' нашли, ищем ту что после '-', начиная с начала списка комнат
-            tmp = map->rooms;
-            while (tmp)
-            {
-                if (ft_strequ(tmp->name, line + len + 1) && tmp->number != start->number)
-                {
-                    // если нашли комнаты запихиваем в список (для каждой комнаты свой)
-                    first = ft_list_i_head(start->number, map->link);
-                    second = ft_list_i_head(tmp->number, map->link);
-                    ft_list_add_back_i_or_exit(&first->next, tmp->number, first->next);
-                    ft_list_add_back_i_or_exit(&second->next, start->number, second->next);
-                    ft_str_print_del(&line);
-                    return (1);
-                }
-                tmp = tmp->next;
-            }
-            return (is_not_valid("not room in link\n"));
-            //return (0);//если комнаты закончились раньше, чем мы её нашли
-        }
-        start = start->next;
-    }// если сюда дошли, значит не нашли первую комнату
-    return (is_not_valid("not room in link\n"));
+int	check_links(char *line, t_map *map)
+{
+	t_room			*s;
+	t_room			*t;
+	int				l;
+	t_list_down		*f;
+	t_list_down		*se;
+
+	s = map->rooms;
+	while (s)
+	{
+		l = ft_strlen(s->name);
+		if (ft_strnequ(s->name, line, l) && ft_strnequ("-", line + l, 1))
+		{
+			t = map->rooms;
+			while (t)
+			{
+				if (ft_strequ(t->name, line + l + 1) && t->number != s->number)
+				{
+					f = ft_list_i_head(s->number, map->link);
+					se = ft_list_i_head(t->number, map->link);
+					ft_list_add_back_i_or_exit(&f->next, t->number, f->next);
+					ft_list_add_back_i_or_exit(&se->next, s->number, se->next);
+					ft_str_print_del(&line);
+					return (1);
+				}
+				t = t->next;
+			}
+			return (is_not_valid("not room in link\n"));
+		}
+		s = s->next;
+	}
+	return (is_not_valid("not room in link\n"));
 }
 
 /*
-копирует список комнат в столбец смежности
-по идее список комнат меняться не будет - можно оставить ссылки (?)
-для каждой line, которая не коммент заполняем ссылки
+**копирует список комнат в столбец смежности
+**по идее список комнат меняться не будет - можно оставить ссылки (?)
+**для каждой line, которая не коммент заполняем ссылки
 */
-int created_links(char *line, t_map *map, t_valid *val_id)
+
+int	created_links(char *line, t_map *map, t_valid *val_id)
 {
-    t_room	*tmp;
+	t_room	*tmp;
 
-    tmp = map->rooms;
-    while (tmp)
-    {
-        ft_list_add_back_down(&map->link, ft_list_new_down(tmp->number));
-        tmp = tmp->next;
-    }
-    //зашла уже с лайном сюда - отдельно обработать приходится
-    check_links(line, map);
-    while ((line = ft_check_cmd(&map->fd)))
-    {
-        if (line[0] == '#')
-            return (is_not_valid("end or start in link\n"));
-        check_links(line, map);
-    }
-    if (!line && val_id->start == 2 && val_id->end == 2)
-    {
-        free(val_id);
-        return (1);
-    }
-    return (is_not_valid("no start or end\n"));
+	tmp = map->rooms;
+	while (tmp)
+	{
+		ft_list_add_back_down(&map->link, ft_list_new_down(tmp->number));
+		tmp = tmp->next;
+	}
+	check_links(line, map);
+	while ((line = ft_check_cmd(&map->fd)))
+	{
+		if (line[0] == '#')
+			return (is_not_valid("end or start in link\n"));
+		check_links(line, map);
+	}
+	if (!line && val_id->start == 2 && val_id->end == 2)
+	{
+		free(val_id);
+		return (1);
+	}
+	return (is_not_valid("no start or end\n"));
 }
-
-
